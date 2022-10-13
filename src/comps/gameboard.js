d9732331c18d7f63719d0c2ba004721889b2044f
SamLinux <oxedom@gmail.com>
2022-10-13 12:33:55 +0300

    added cameras to cards array

--- src/comps/gameboard.js
+++ src/comps/gameboard.js
@@ -1,13 +1,58 @@
 import useState from 'react'
+import uniqid from 'uniqid'
 
 const Gameboard = () => {
 
 
     const [currentScore, setCurrentScore] = useState(0)
     const [highScore, setHighScore] = useState(0)
+    const cards =
+        [
+            {
+                id: uniqid(),
+                name: 'Minolta X300'
+            },
+            {
+                id: uniqid(),
+                name: 'hasselblad 500c'
+            },
+            {
+                id: uniqid(),
+                name: 'Fuji 69'
+            },
+            {
+                id: uniqid(),
+                name: 'Pentax 67'
+            },
+            {
+                id: uniqid(),
+                name: 'Lecia iiif'
+            },
+            {
+                id: uniqid(),
+                name: 'Maymia RB67'
+            }
+            ,
+            {
+                id: uniqid(),
+                name: 'Olympus Trip'
+            }
+            , {
+                id: uniqid(),
+                name: 'Canon AE1'
+            },
+            {
+                id: uniqid(),
+                name: 'Pentax K1000'
+            }
+        ]
 
 
-    return (<main></main>);
+
+
+    return (<main>
+
+    </main>);
 }
 
 export default Gameboard;

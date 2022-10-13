a34e1d8b855bc286698ecd43e9f2443a24dfafe5
SamLinux <oxedom@gmail.com>
2022-10-13 14:31:28 +0300

    added shuffling and implementing a score tracker

--- src/comps/gameboard.js
+++ src/comps/gameboard.js
@@ -9,86 +9,95 @@ const Gameboard = () => {
     const [highScore, setHighScore] = useState(0);
     const [clicked, setClicked] = useState([])
 
+    const [cards, setCards] = useState([
+        {
+            id: 1,
+            name: 'Minolta X300'
+        },
+        {
+            id: 2,
+            name: 'hasselblad 500c'
+        },
+        {
+            id: 3,
+            name: 'Fuji 69'
+        },
+        {
+            id: 4,
+            name: 'Pentax 67'
+        },
+        {
+            id: 5,
+            name: 'Lecia iiif'
+        },
+        {
+            id: 6,
+            name: 'Maymia RB67'
+        }
+        ,
+        {
+            id: 7,
+            name: 'Olympus Trip'
+        }
+        , {
+            id: 8,
+            name: 'Canon AE1'
+        },
+        {
+            id: 9,
+            name: 'Pentax K1000'
+        },
+        {
+            id: 10,
+            name: 'Nikon F'
+        },
+        {
+            id: 11,
+            name: 'Rollieflex'
+        },
+        {
+            id: 12,
+            name: 'Lecia M6'
+        },
+        // {
+        //     id: uniqid(),
+        //     name: 'Holga 120GN'
+        // }
+    ])
+
+
     const addClicked = (id) => {
         setClicked((previousState) => {
             return [...previousState, id]
         })
     }
 
-
     const handleGameOver = () => {
-        alert('gameover', highScore)
+        alert(currentScore)
+
+
+        // setCurrentScore(0)
     }
 
 
 
     useEffect(() => {
-
+        //Shuffles Array to make memory game harder
+        function shuffle(array) { return array.sort(() => Math.random() - 0.5); }
+        //Checks the Clicked Array for Duplicates
         function hasDuplicates(array) { return (new Set(array)).size !== array.length; }
 
+        setCards(shuffle(cards))
+
         if (hasDuplicates(clicked)) { handleGameOver() }
+        else { setCurrentScore((prevState) => { return prevState + 1 }) }
 
     }, [clicked]);
 
 
 
 
-    const cards =
-        [
-            {
-                id: 1,
-                name: 'Minolta X300'
-            },
-            {
-                id: 2,
-                name: 'hasselblad 500c'
-            },
-            {
-                id: 3,
-                name: 'Fuji 69'
-            },
-            {
-                id: 4,
-                name: 'Pentax 67'
-            },
-            {
-                id: 5,
-                name: 'Lecia iiif'
-            },
-            {
-                id: 6,
-                name: 'Maymia RB67'
-            }
-            ,
-            {
-                id: 7,
-                name: 'Olympus Trip'
-            }
-            , {
-                id: 8,
-                name: 'Canon AE1'
-            },
-            {
-                id: 9,
-                name: 'Pentax K1000'
-            },
-            {
-                id: 10,
-                name: 'Nikon F'
-            },
-            {
-                id: 11,
-                name: 'Rollieflex'
-            },
-            {
-                id: 12,
-                name: 'Lecia M6'
-            },
-            // {
-            //     id: uniqid(),
-            //     name: 'Holga 120GN'
-            // }
-        ]
+
 
 
     return (
